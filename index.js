079a09702c50e49ff079e4bdcb29abe9e6a350f5
Diana Tinoco <dtinoco152@gmail.com>
2021-11-04 08:05:49 -0600

    S23 Logeando el objeto request

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ import http from 'http';
 // (request, renponse)
 const server = http.createServer((req, res) => {
     console.log("> Se ha recibido una peticion.");
+    // logeando el objeto req: peticion
+    console.log(`Informacion de la Peticion`)
+    console.log(`url: ${req.url}`)
+    console.log(`Request Method: ${req.method}`)
+    console.log(`Plataforma del Cliente: ${req.headers["sec-ch-ua-plataform"]}`)
     // Respondemos
-    res.write('Hola');
+    res.write('Respuesta del Servidor.');
     // Terminar la conexion
     res.end();
 });
 
 // 3. Pongo a trabajar el servidor
-server.listen(3000, '192.168.100.6', () => {
-    console.log("Servidor escuchando en http://192.168.100.6:3000")
+// le paso un callback que escribira en la consola
+// cuando el servidor este escuchando
+//192.168.100.6:3000
+server.listen(3000, '0.0.0.0', () => {
+    console.log("Servidor escuchando en http://localhost:3000")
 });
 
 
