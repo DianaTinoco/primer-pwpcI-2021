4694f2fd8c9b5aeda7b9871f6674a2e086a37fb8
Diana Tinoco <dtinoco152@gmail.com>
2021-11-23 08:51:19 -0600

    S30 Middleware con Express

--- index.js
+++ index.js
@@ -10,14 +10,28 @@ import Express  from 'express'
 // Crear una instancia de Express
 const app = Express(); // (req, resp, next)=>{ } event handler
 
-// 2. Crear el servidor tomando como
-// manejador de peticiones a express
-const server = http.createServer(app);
+// Registrando el primer middleware
+app.use((req, res, next)=>{
+    // Registrar un mensaje en el log
+    console.log("Estoy en el middleware 1");
+    // Dar instruccion de pasar al siguiente middleware
+    next()
+});
+// Regpistrando el segundo middleware
+app.use((req, res, next)=>{
+    // Registrar un mensaje en el log
+    console.log("Estoy en el middleware 2");
+    // Dar instruccion de pasar al siguiente middleware
+    next()
+});
 
-// 3. Pongo a trabajar el servidor
-// le paso un callback que escribira en la consola
-// cuando el servidor este escuchando
-//192.168.100.6:3000
-server.listen(3000, '192.168.100.6', () => {
-    console.log("Servidor escuchando en http://192.168.100.6:3000")
-}); 
+app.use((_, res)=>{
+    console.log("Estoy en el middleware 3");
+    console.log("Emitiendo respuesta a cliente");
+    res.send("<h1>Mi respuesta</h1>\n Hola");
+});
+
+// Poniendo a escuchar la app de express
+app.listen(3000, '192.168.100.6',( ) => {
+    console.log("Servidor escuchando en http://192.168.100.6:3000");
+});
