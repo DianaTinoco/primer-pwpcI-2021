c5eb7c6ccf171d2677b4f1854cf5b92dd687b26a
Diana Tinoco <dtinoco152@gmail.com>
2021-11-25 09:11:47 -0600

    S30 Administrando diferentes rutas

--- index.js
+++ index.js
@@ -10,54 +10,20 @@ import Express  from 'express'
 // Crear una instancia de Express
 const app = Express(); // (req, resp, next)=>{ } request handler
 
-// Registrando el primer middleware
-app.use((req, res, next)=>{
-    // Registrar un mensaje en el log
-    console.log("Estoy en el middleware 1");
-    // Dar instruccion de pasar al siguiente middleware
-    next()
+// La posicion en que se encuentran es importante
+// Se debe colocar primero ya que el orden de registro
+//determina el orden de verificacion 
+app.use('/about',(_, res)=>{
+    console.log('Se ha realizado la peticion "/about"');
+    res.send("<h1>Acerca de...</h1>\n Sitio inicial hecho en NodeJs");
 });
 
-// Regpistrando el segundo middleware
-app.use((req, res, next)=>{
-    // Registrar un mensaje en el log
-    console.log("Estoy en el middleware 2");
-    next()
-});
-
-app.use((_, res)=>{
-    console.log("Estoy en el middleware 3");
-    console.log("Emitiendo respuesta a cliente");
-    res.send("<h1>Mi respuesta</h1>\n Hola");
+app.use('/',(_, res)=>{
+    console.log('Se ha realizado la petici on: "/"');
+    res.send("<h1>Mi App</h1>\n Bienvenido a este sitio");
 });
 
 // Poniendo a escuchar la app de express
 app.listen(3000,'192.168.100.6',()=>{
     console.log("Servidor escuchando en http://192.168.100.6:3000")
-});
-
-// Registrando el primer middleware
-app.use((req, res, next)=>{
-    // Registrar un mensaje en el log
-    console.log("Estoy en el middleware 1");
-    // Dar instruccion de pasar al siguiente middleware
-    next()
-});
-// Regpistrando el segundo middleware
-app.use((req, res, next)=>{
-    // Registrar un mensaje en el log
-    console.log("Estoy en el middleware 2");
-    // Dar instruccion de pasar al siguiente middleware
-    next()
-});
-
-app.use((_, res)=>{
-    console.log("Estoy en el middleware 3");
-    console.log("Emitiendo respuesta a cliente");
-    res.send("<h1>Mi respuesta</h1>\n Hola");
-});
-
-// Poniendo a escuchar la app de express
-app.listen(3000, '192.168.100.6',( ) => {
-    console.log("Servidor escuchando en http://192.168.100.6:3000");
 });
