7658c76c13775817572bcf6f01cf7b8539023dc7
Diana Tinoco <dtinoco152@gmail.com>
2021-11-25 15:10:50 -0600

    S31 Procesando un formulario

--- index.js
+++ index.js
@@ -10,19 +10,52 @@ import Express  from 'express'
 // Crear una instancia de Express
 const app = Express(); // (req, resp, next)=>{ } request handler
 
+// 1. Insertando Middleware para la lectura de datos
+// de un cliente 
+app.use(Express.urlencoded({extended: false}));
+
+// Loggin de peticiones
+app.use((req,_,next)=>{
+    console.log(`Se ha realizado la peticion: "${req.method} : ${req.path}"`);
+    next();
+});
+
+
 // La posicion en que se encuentran es importante
 // Se debe colocar primero ya que el orden de registro
 //determina el orden de verificacion 
 app.use('/about',(_, res)=>{
-    console.log('Se ha realizado la peticion "/about"');
     res.send("<h1>Acerca de...</h1>\n Sitio inicial hecho en NodeJs");
+});
+
+// Primera prueba
+app.use('/add-student-form',(_,res)=>{
+    res.send(`
+	<form action="/add-student" method="POST">
+	<label for="student_name"> Student Name</label>
+	<input type="text" name="name" id="student-name">
+	<button type="submit">Add student</button>
+	</form>
+	`);
+});
+
+// Ruta que procesa el formulario
+app.use('/add-student', (req, res, next) => {
+    // Iterando sobre todo el objeto
+    for(const prop in req.body){
+        console.log(`${prop}: ${req.body[prop]}`);
+    }
+    console.log(`Metodo: ${req.method}`);
+
+    res.json(req.body);
+    // Realizamos un redireccionamiento
+	//res.redirect('/');
 });
 
 // La ruta raiz entra en todo tipo de peticion
 // raiz siempre se va al final 
 // Asociacion de dos rutas '/' y '/home'
 app.use(['/','/home'],(_, res)=>{
-    console.log('Se ha realizado la peticion: "/"');
     res.send("<h1>Mi App</h1>\n Bienvenido a este sitio");
 });
 
